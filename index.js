c305050d4f6c31a8863faae67b3dd20456bee167
YoonHoJeong <jack58@naver.com>
2020-01-23 16:23:58 +0900

    to 2-7
    nomad 2-7까지 코드 변경사항.

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
+// html에선 html, css파일에선 css, js에선 로직만 담당하도록 변경.
+
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
+  // 클래스 자체를 변경시켜서
+  // const currentClass = title.className;
+
+  // class name을 받아옴.
+  // console.log(currentClass);
+  // if (currentClass !== CLICKED_CLASS) {
+  //   // class를 변경시킴.
+  //   title.className = CLICKED_CLASS;
+  // } else {
+  //   title.className = "";
+  // }
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+
+  // if (hasClass) {
+  //   // class를 삭제시킴.
+  //   title.classList.remove(CLICKED_CLASS);
+  // } else {
+  //   // class를 추가.
+  //   title.classList.add(CLICKED_CLASS);
+  // }
+
+  // 위와 같은 기능을 똑같이 수행.
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
-  // window를 resize했을 때 handleResize 함수를 호출한다.
   window.addEventListener("click", handleClick);
 }
 
