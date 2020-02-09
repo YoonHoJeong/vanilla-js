c1621672d1a526527fcab41a5f74e6d9153d0c00
YoonHoJeong <jack58@naver.com>
2020-02-09 12:00:12 +0900

    update

--- todo.js
+++ todo.js
@@ -43,11 +43,12 @@ function paintToDo(text) {
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
+
   delBtn.innerText = "X";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
-  li.appendChild(delBtn);
   li.appendChild(span);
+  li.appendChild(delBtn);
   li.id = newId;
   toDoList.appendChild(li);
   const toDoObj = {
